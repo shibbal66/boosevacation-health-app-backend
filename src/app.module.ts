47fcdd027197e9c96ac48b6140a110c0763da87d
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2026-03-11 00:02:38 +0500

    App Module Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,34 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "src/app.controller";
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { LoggerMiddleware } from "middleware/logger.middleware";
+import { DatabaseModule } from "modules/database/database.module";
+import { HashModule } from "modules/hash/hash.module";
+import { JWTModule } from "modules/jwt/jwt.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController]
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [{ ttl: 60000, limit: 50 }]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    }),
+    DatabaseModule,
+    JWTModule,
+    HashModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes("*");
+  }
+}
